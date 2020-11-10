74fe020f7b5410910f5c9c39433e0b7146ecdbe3
Pai <abhin.pai@honeywell.com>
2020-11-11 01:02:14 +0530

    Updated the code

--- server/middleware/checkObjectId.js
+++ server/middleware/checkObjectId.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 import { validationErrorWithData } from '../helpers/apiResponse'
 
-const checkObjectId = objectId => (req, res, next) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params[objectId]))
-    return validationErrorWithData(res, 'Invalid Id', null)
+const checkObjectId = (req, res, next, id) => {
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return validationErrorWithData(res, 'Invalid Object Id', null) 
   next()
 }
 
--- server/routes/dealers.js
+++ server/routes/dealers.js
@@ -4,37 +4,24 @@ import * as DealersController from '../controllers/dealers'
 import validateToken from '../middleware/tokenValidator'
 const router = express.Router()
 
+router.param('dealerId', checkObjectId)
+
 // POST /v1/api/dealers/single
 router.post('/single', validateToken, DealersController.saveDealer)
 
 // POST /v1/api/dealers/bulk
 router.post('/bulk', validateToken, DealersController.saveDealers)
 
-// GET /v1/api/dealers/:id
-router.get(
-  '/:id',
-  validateToken,
-  checkObjectId('id'),
-  DealersController.getDelaerById
-)
+// GET /v1/api/dealers/:dealerId
+router.get('/:dealerId', validateToken, DealersController.getDelaerById)
 
 // GET /v1/api/dealers/
 router.get('/', validateToken, DealersController.getAllDealers)
 
-// DELETE /v1/api/dealers/:id
-router.delete(
-  '/:id',
-  validateToken,
-  checkObjectId('id'),
-  DealersController.deleteById
-)
+// DELETE /v1/api/dealers/:dealerId
+router.delete('/:dealerId', validateToken, DealersController.deleteById)
 
-// PUT /v1/api/dealers/:id
-router.put(
-  '/:id',
-  validateToken,
-  checkObjectId('id'),
-  DealersController.updateById
-)
+// PUT /v1/api/dealers/:dealerId
+router.put('/:dealerId', validateToken, DealersController.updateById)
 
 export default router
