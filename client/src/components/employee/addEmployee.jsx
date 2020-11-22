992d917d564c48a877eb8985d3fc236cba89231d
Abhin Pai <abhinpai96@gmail.com>
2020-11-22 16:42:10 +0530

    Added the table to show the employee

--- client/src/components/employee/addEmployee.jsx
+++ client/src/components/employee/addEmployee.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { SET_SHOW_ADD_EMPLOYEE } from '../../state/action';
+import React, { useState } from 'react';
+import {
+  ActionAddEmpData,
+  ActionAddEmployeeUI,
+  SET_SHOW_ADD_EMPLOYEE,
+} from '../../state/action';
+import useData from '../../state/dataLayer';
 import { EmployeeRoles } from '../../utils/constants';
 import Drawer from '../drawer/drawer';
+import { successToast } from '../notification/notification';
 
 function AddEmployee() {
-  const addEmployee = () => console.log('Add Employee action');
+  const [empData, setEmpData] = useState({
+    name: null,
+    phone: null,
+    email: null,
+    role: null,
+    password: null,
+  });
+  const [, dispatch] = useData();
+  const addEmployee = () => {
+    ActionAddEmpData(dispatch, empData);
+    ActionAddEmployeeUI(dispatch, false);
+    successToast({
+      title: 'Adding Employee',
+      message: 'Employee added successfully',
+    });
+  };
 
   return (
     <Drawer
@@ -12,11 +33,33 @@ function AddEmployee() {
       btnLabel='Add'
       btnAction={addEmployee}
       actionType={SET_SHOW_ADD_EMPLOYEE}>
-      <input type='text' placeholder='Name' />
-      <input type='email' placeholder='Email Id' />
-      <input type='bumber' placeholder='Phone No' />
-      <input type='password' placeholder='Password' />
-      <select name='Select role' id='emp-role'>
+      <input
+        type='text'
+        placeholder='Name'
+        onChange={e => setEmpData({ ...empData, name: e.target.value })}
+      />
+      <input
+        type='email'
+        placeholder='Email Id'
+        onChange={e => setEmpData({ ...empData, email: e.target.value })}
+      />
+      <input
+        type='tel'
+        placeholder='Phone No'
+        onChange={e => setEmpData({ ...empData, phone: e.target.value })}
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        onChange={e => setEmpData({ ...empData, password: e.target.value })}
+      />
+      <select
+        name='Select role'
+        id='emp-role'
+        onChange={e => setEmpData({ ...empData, role: e.target.value })}>
+        <option value='Select Role' key='roles'>
+          Select Role
+        </option>
         {EmployeeRoles.map((role, index) => (
           <option value={role.name} key={index}>
             {role.name}
